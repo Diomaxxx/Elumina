5fb911b5a8f078ea1a1fab84b0f7db3eb7ac6cc5
Diomaxxx <diogofrancounivalle@gmail.com>
2024-12-03 13:52:12 -0400

    Perfil

--- app/src/main/java/com/example/elumina/PerfilClienteActivity.java
+++ app/src/main/java/com/example/elumina/PerfilClienteActivity.java
@@ -1,20 +1,25 @@
 package com.example.elumina;
 
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowInsets;
 import android.view.WindowInsetsController;
-import android.widget.Button;
-import android.widget.LinearLayout;
-
+import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
-public class PerfilClienteActivity  extends AppCompatActivity {
+import com.example.elumina.Graphql.Models.Entity.Cliente;
+import com.example.elumina.Graphql.Repositories.ClienteRepository;
+import com.example.elumina.Graphql.Service.GraphQLCallback;
 
+public class PerfilClienteActivity extends AppCompatActivity {
+
+    private ClienteRepository clienteRepository;
+
+    private TextView name, lastname, address, email, phone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +27,45 @@ public class PerfilClienteActivity  extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.cliente_perfil);
 
+        name = findViewById(R.id.textViewNameValue);
+        lastname = findViewById(R.id.textViewLastNameValue);
+        address = findViewById(R.id.textViewAddressValue);
+        email = findViewById(R.id.textViewEmailValue);
+        phone = findViewById(R.id.textViewPhoneValue);
+
+
+        clienteRepository = new ClienteRepository();
+        SharedPreferences sharedPreferences = getSharedPreferences("user_prefs", MODE_PRIVATE);
+        String token = sharedPreferences.getString("token", null);
+
+        if (token != null) {
+            getData(token);
+        } else {
+            Log.e("PerfilCliente", "Token no encontrado en SharedPreferences");
+        }
     }
 
-    // Método para ocultar botones de navegación y barra de estado
+    private void getData(String token) {
+        clienteRepository.obtenerClienteDesdeToken(token, new GraphQLCallback<Cliente>() {
+            @Override
+            public void onSuccess(Cliente cliente) {
+                Log.d("Cliente", "Cliente recibido: " + cliente.getId());
+
+                name.setText(cliente.getNombre());
+                lastname.setText(cliente.getApellido());
+                email.setText(cliente.getCorreo());
+                phone.setText(cliente.getTelefono());
+                address.setText(cliente.getDireccion());
+
+            }
+
+            @Override
+            public void onError(String errorMessage) {
+                Log.e("ErrorCliente", "Error al obtener cliente: " + errorMessage);
+            }
+        });
+    }
+
     private void hideSystemUI() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             getWindow().setDecorFitsSystemWindows(false);
@@ -52,6 +93,4 @@ public class PerfilClienteActivity  extends AppCompatActivity {
             hideSystemUI(); // Restaurar modo de inmersión al recuperar el foco
         }
     }
-
 }
-
